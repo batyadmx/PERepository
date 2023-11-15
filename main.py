cd2d8d554cef54dbc5b9ecbf3636fab84a56ea18
Цветков Максим Вадимович <tsmv@skbkontur.ru>
2023-11-15 21:35:51 +0500

    web-application

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import torch
+import streamlit as st
 from TTS.api import TTS
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-tts = TTS(model_name="tts_models/en/ljspeech/speedy-speech", progress_bar=False).to(device)
-tts.tts_to_file(text="MAXIM AND SANYA.", file_path="output.wav")
+def main():
+    st.title("TEXT TO SPEECH")
+
+    text_input = st.text_area("Введите ваш текст здесь. Текст должен быть на английском!", height=200)
+    
+    if st.button("Перевести в аудио"):
+        download_text_file(text_input)
+
+def download_text_file(text_content):
+    audio_file = "output.wav"
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tts = TTS(model_name="tts_models/en/ljspeech/speedy-speech", progress_bar=False).to(device)
+    tts.tts_to_file(text=text_content, file_path=audio_file)
+
+    with open(audio_file, "rb") as file:
+        file_content = file.read()
+
+    st.download_button(
+        label="Скачать файл",
+        key="text_download_button",
+        file_name=audio_file,
+        data=file_content
+    )
+
+if __name__ == "__main__":
+    main()
